847e64987bcce14312934c4c240ca331b8042a8e
Emma Slight <emma.slight@icloud.com>
2020-12-05 21:57:58 +1300

    add click eventlistener for seats

--- script.js
+++ script.js
@@ -5,3 +5,15 @@ const total = document.getElementById('total')
 const movieSelect = document.getElementById('movie')
 
 let ticketPrice = +movieSelect.value
+
+container.addEventListener('click', (e) => {
+  console.log(e.target.classList)
+
+  if (
+    e.target.classList.contains('seat') &&
+    !e.target.classList.contains('occupied')
+  ) {
+    e.target.classList.toggle('selected')
+  }
+  updateSelectedCount()
+})
